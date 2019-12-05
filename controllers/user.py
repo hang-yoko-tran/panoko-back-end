2b1823cb6ea589cad52382e1d6a2fe4e214c7b13
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-12-05 15:40:56 +0700

    Implement Forgot password

--- controllers/user.py
+++ controllers/user.py
@@ -76,39 +76,50 @@ def send_email(token, email, name):
                              data={"from": "Hang Yoko <hang.yoko.tran@gmail.com>",
                                    "to": [email],
                                    "subject": "Reset Password",
-                                   "text": f"Go to http://localhost:5000/user/new-password/{token}"})
+                                   "text": f"Go to https://localhost:3000/new-password/{token}"})
 
     print(response)
 
 @user_blueprint.route('/forgot-password', methods=['GET', 'POST'])
 def forgot_password():
-    if current_user.is_authenticated:
-        return redirect(url_for('root'))
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('root'))
     if request.method == 'POST':
-        email = request.form['email']
+        email = request.get_json()['email']
         user = User.query.filter_by(email=email).first()
         if not user:
-            flash('ACCOUTN DOES NOT EXIST', 'danger')
-            return redirect(url_for('user_bp.forgot_password'))
+            # flash('ACCOUTN DOES NOT EXIST', 'danger')
+            # return redirect(url_for('user_bp.forgot_password'))
+            print("Email not exist!")
+            return jsonify({"code": 404})
         s = URLSafeTimedSerializer(app.secret_key)
         token = s.dumps(user.email, salt="UMEO")       
         name = f'{user.firstname} {user.lastname}'
+        print("TOKEN:", token)
+        print("EMAIL", user.email)
         send_email(token, user.email, name)
-    return render_template('user/forgot_password.html')
+       
+        return jsonify({"code": 200})
+    # return ('user/forgot_password.html')
 
-@user_blueprint.route('/new-password/<token>', methods=['GET', 'POST'])
+@user_blueprint.route('/new-password/<token>', methods=['POST'])
 def new_password(token):
-    s = URLSafeTimedSerializer(app.secret_key)
-    email = s.loads(token, salt="UMEO", max_age=180)
-    user = User.query.filter_by(email=email).first()
-    if not user:
-        flash("INVALID TOKEN", "danger")
-        return redirect(url_for('root'))
+    print("TOKEN: ", token)
+    try:
+        s = URLSafeTimedSerializer(app.secret_key)
+        email = s.loads(token, salt="UMEO", max_age=3000)
+    except:
+        print("Invalid token")
+        return jsonify({"code": 404})
+    
+    print("EMAIL", email)
+        
     if request.method == "POST":
-        if request.form['new_password'] != request.form['confirm_password']:
-            flash('Password not match!', 'warning')
-        user.set_password(request.form['new_password'])
+        if request.get_json()['password'] != request.get_json()['confirm_password']:
+            print('Password not match!')
+            return jsonify({"code": 400})
+        user = User.query.filter_by(email=email).first()
+        user.set_password(request.get_json()['password'])
         db.session.commit()
         flash("You have set new password", "successful")
-        return redirect(url_for('user_bp.login'))
-    return render_template('user/reset_password.html', token=token)
+        return jsonify({"code": 200})
